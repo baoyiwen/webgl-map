787475d6919d338ff478ab6e6769449a872407f3
baoyiwen <511530203@qq.com>
2024-03-03 11:38:51 +0800

    update commpont

--- src/components/webgl.vue
+++ src/components/webgl.vue
@@ -4,24 +4,50 @@
 
 <script lang="ts">
 type c_webgl_props = {
-    target: String;
-}
-import {Options, Vue} from "vue-class-component";
+  target: String;
+};
+import { Options, Vue } from "vue-class-component";
 @Options({
-    prop: {
-        target: [String, HTMLElement],
-    }
+  prop: {
+    target: [String, HTMLElement],
+  },
 })
-export default class Webgl extends Vue { 
-    target!: String | HTMLElement;
-    constructor(props: c_webgl_props) {
-        super(props)
-    }
+export default class Webgl extends Vue {
+ target!: String | HTMLElement;
+  data() {
+    return {};
+  }
 
-    mounted() {
-    }
+  // 组件的内部对象
+  components: {};
+  // 构造函数，用于初始化组件
+  constructor(props: c_webgl_props) {
+    super(props);
+  }
 
-    
+  // 计算属性，用于计算组件的内部数据
+  computed: {};
+
+  // 组件挂载完成时调用
+  mounted() {}
+
+  // 组件创建完成时调用
+  created() {}
+
+  // 组件销毁时调用
+  destroyed() {}
+
+  // 组件更新时调用
+  updated() {}
+
+  // 渲染组件
+  render() {}
+
+  // 组件方法
+  methods: {};
+
+  // 组件观察属性
+  watch: {};
+
 }
-
 </script>
