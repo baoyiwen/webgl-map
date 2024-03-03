5c6f0623f5e7aef3bfe9b8dee110a1767cda46a0
baoyiwen <511530203@qq.com>
2024-03-03 11:15:04 +0800

    create code and layout

---
+++ src/components/webgl.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+type c_webgl_props = {
+    target: String;
+}
+import {Options, Vue} from "vue-class-component";
+@Options({
+    prop: {
+        target: [String, HTMLElement],
+    }
+})
+export default class Webgl extends Vue { 
+    target!: String | HTMLElement;
+    constructor(props: c_webgl_props) {
+        super(props)
+    }
+
+    mounted() {
+    }
+
+    
+}
+
+</script>
