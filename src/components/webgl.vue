19a4cda787602100ca285068737c91a32e9941d9
baoyiwen <511530203@qq.com>
2024-03-04 12:38:03 +0800

    update pamer

--- src/components/webgl.vue
+++ src/components/webgl.vue
@@ -1,99 +1,105 @@
 <template>
-  <div class="canvas-root">
-    <canvas class="canvas-context" ref="canvasElements"></canvas>
-  </div>
+	<div class="canvas-root">
+		<canvas
+			class="canvas-context"
+			ref="canvasElements"
+		>
+		</canvas>
+	</div>
 </template>
 
 <script lang="ts">
-type c_webgl_props = {
-  target: String | HTMLElement;
-  width: Number;
-  height: Number;
-};
-import { Options, Vue } from "vue-class-component";
-import { ref, Ref } from "vue";
-@Options({
-  prop: {
-    target: [String, HTMLElement],
-    width: Number,
-    height: Number,
-  },
-})
-export default class Webgl extends Vue {
-  target!: String | HTMLElement;
-  width!: number;
-  height!: number;
+	type c_webgl_props = {
+		target: string | HTMLElement;
+		width: number;
+		height: number;
+	};
+	import { Options, Vue } from "vue-class-component";
+	import { ref, Ref } from "vue";
+	@Options({
+		prop: {
+			target: [String, HTMLElement],
+			width: Number,
+			height: Number,
+		},
+	})
+	export default class Webgl extends Vue {
+		target!: string | HTMLElement;
+		width!: number;
+		height!: number;
 
-  private canvasElements!: Ref<HTMLCanvasElement | null>;
-  private mutationObserver!: MutationObserver;
-  // 构造函数，用于初始化组件
-  constructor(props: c_webgl_props) {
-    super(props);
-    this.init();
-    this.canvasElements = ref<HTMLCanvasElement | null>(null);
-    this.mutationObserver = new MutationObserver((ele) => {
-      this.getCanvasWarpSize();
-    });
-  }
+		private canvasElements!: Ref<HTMLCanvasElement | null>;
+		private mutationObserver!: MutationObserver;
+		// 构造函数，用于初始化组件
+		constructor(props: c_webgl_props) {
+			super(props);
+			this.init();
+			this.canvasElements = ref<HTMLCanvasElement | null>(null);
+			this.mutationObserver = new MutationObserver((ele) => {
+				this.getCanvasWarpSize();
+			});
+		}
 
-  // 初始化组件
-  init() {
-    // 初始化画布和webgl上下文
-    this.width = 500;
-    this.height = 500;
-  }
+		// 初始化组件
+		init() {
+			// 初始化画布和webgl上下文
+			this.width = 500;
+			this.height = 500;
+		}
 
-  private get dealWidth() {
-    return this.width - 20;
-  }
+		private get dealWidth() {
+			return this.width - 20;
+		}
 
-  private get dealHeight() {
-    return this.height - 20;
-  }
+		private get dealHeight() {
+			return this.height - 20;
+		}
 
-  private getCanvasWarpSize() {}
+		private getCanvasWarpSize() {
+			return null;
+		}
 
-  // 组件挂载完成时调用
-  mounted() {}
+		// 组件挂载完成时调用
+		mounted() {}
 
-  beforeMount(): void {
-    // // 监听目标元素的变化
-    // this.mutationObserver.observe(this.target, {
-    //   attributes: true, // 监听属性变化
-    //   attributeFilter: ["style", "class"], // 监听特定属性
-    //   childList: true, // 监听子节点变化
-    //   subtree: true, // 监听后代节点变化
-    // });
-  }
+		beforeMount(): void {
+			// // 监听目标元素的变化
+			// this.mutationObserver.observe(this.target, {
+			//   attributes: true, // 监听属性变化
+			//   attributeFilter: ["style", "class"], // 监听特定属性
+			//   childList: true, // 监听子节点变化
+			//   subtree: true, // 监听后代节点变化
+			// });
+		}
 
-  // 组件创建完成时调用
-  created() {}
+		// 组件创建完成时调用
+		created() {}
 
-  // 组件销毁时调用
-  destroyed() {
-    // 移除监听
-    this.mutationObserver.disconnect();
-  }
+		// 组件销毁时调用
+		destroyed() {
+			// 移除监听
+			this.mutationObserver.disconnect();
+		}
 
-  // 组件更新时调用
-  updated() {}
+		// 组件更新时调用
+		updated() {}
 
-  // 渲染组件
-  render() {}
+		// 渲染组件
+		render() {}
 
-  // 组件观察属性
-}
+		// 组件观察属性
+	}
 </script>
 
 <style scoped lang="less">
-.canvas-root {
-  width: 100%;
-  height: 100%;
-  box-sizing: border-box;
-}
+	.canvas-root {
+		width: 100%;
+		height: 100%;
+		box-sizing: border-box;
+	}
 
-.canvas-context {
-  width: 100%;
-  height: 100%;
-}
+	.canvas-context {
+		width: 100%;
+		height: 100%;
+	}
 </style>
---
+++ src/views/Layout.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="root-panel"></div>
+</template>
+<script lang="ts">
+	import Webgl from "../components/webgl.vue";
+	import { Options, Vue } from "vue-class-component";
+	@Options({
+		components: {
+			Webgl,
+		},
+	})
+	export default class Layout extends Vue {
+		constructor() {
+			super();
+		}
+	}
+</script>
+<style lang="less" scoped></style>
